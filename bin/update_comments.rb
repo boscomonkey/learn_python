f87b4f1862c03ec7ef2e7fd5a7a0206b8fb04375
Bosco So <bso@alum.mit.edu.nospam>
2018-04-18 18:12:05 -0400

    updated Hello Friend comment to use Python "rerun" package

--- bin/update_comments.rb
+++ bin/update_comments.rb
@@ -7,17 +7,18 @@
 #	rerun -p '*.py' -cx -- ./test_pig_latin.py -v
 =end
 
-re = %r(^#\s*Example rerun command:\n#\s+rerun -p.+(\./test_[\w_]+\.py) -v\n)
+re = %r(^#\s*Example rerun command:\n#\s+rerun -p.+(python\s+)((\.\/)?test_[\w_]+\.py) -v\n)
 str = ARGF.read
 
 m = str.match(re)
 if m
+  fname = m[2]
   replacement = <<EOF
 # This driver is executable from the command line.  Use the Python
 # "rerun" package to re-run the driver whenever files in this
 # directory (and its recursive children) are changed or added.
 #
-#   rerun -v #{m[1]}
+#   rerun -v #{fname}
 EOF
   puts str.sub(re, replacement)
 else
